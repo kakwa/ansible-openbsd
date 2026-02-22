dc2d0a036f7b8af49d6bf61edf4435f98576945f
kakwa <carpentier.pf@gmail.com>
2026-02-22 14:20:54 +0100

    re-authorize social networks

--- roles/openbsd-dns/files/update_denied_domains.sh
+++ roles/openbsd-dns/files/update_denied_domains.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-VARIANT="fakenews-gambling-porn-social"
+VARIANT="fakenews-gambling-porn"
 URL="https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/$VARIANT/hosts"
 wget -qO- "$URL" | grep '^0\.0\.0\.0' | sort | awk '{print "local-zone: \""$2"\" refuse"}' >`dirname $0`/adblock.conf
